b31dca4866130f8e6b7749ec45771219e091b88a
Josh Levinger <josh@levinger.net>
2020-06-05 10:40:51 -0700

    initial commit
    based off gatsby-remark-oembed md-site by raae
    grabs video entries from PoliceBrutality.io and geocodes

---
+++ gatsby-config.js
@@ -0,0 +1,94 @@
+require("dotenv").config();
+
+const { GEOCODER_KEY } = process.env;
+
+module.exports = {
+  siteMetadata: {
+    title: "ReplaceThePolice.org",
+    author: "jlev",
+    description: "A site documenting police brutality in America in 2020",
+    siteUrl: "https://replacethepolice.org/"
+  },
+  pathPrefix: "/",
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/posts`,
+        name: "posts"
+      }
+    },
+    {
+      resolve: 'gatsby-source-apiserver',
+      options: {
+        url: `http://api.policebrutality.io/v1/videos`,
+        method: `get`,
+        auth: false,
+        name: 'PoliceBrutalityVideo',
+        entityLevel: `data`,
+        schemaType: {
+            date: `Date`,
+            name: `String`,
+            state: `String`,
+            city: `String`,
+            links: [`String`]
+        },
+      },
+    },
+    {
+      resolve: `gatsby-transformer-opencage-geocoder`,
+      options: {
+        api_key: GEOCODER_KEY,
+        nodeTypes: [
+          {
+            nodeType: `PoliceBrutalityVideo`,
+            addressFields: ['city', 'state'],
+            addFullResult: false,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `@raae/gatsby-remark-oembed`,
+            options: {
+              providers: {
+                settings: {
+                  Twitter: {
+                    theme: "dark" // Use the Twitter dark theme
+                  },
+                  Instagram: {
+                    hidecaption: true
+                  }
+                }
+              }
+            }
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590
+            }
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`
+            }
+          }
+        ]
+      }
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: "gatsby-plugin-typography",
+      options: {
+        pathToConfigModule: "src/utils/typography"
+      }
+    }
+  ]
+};
