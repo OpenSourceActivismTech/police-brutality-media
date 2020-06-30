66f5c8c0034eef324041dfc7f3cf302146abf902
Josh Levinger <josh@levinger.net>
2020-06-30 15:53:24 -0700

    share open links in new window

--- src/components/Share.js
+++ src/components/Share.js
@@ -4,17 +4,17 @@ import { FaFacebookSquare, FaTwitterSquare, FaWhatsappSquare } from 'react-icons
 const Share = ({ url }) => (
   <div id="share">
     <div class="social-link">
-      <a href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`}>
+      <a href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`} target="_blank" rel="noreferrer">
         <FaFacebookSquare size={32} color={"#3b5998"} />
       </a>
     </div>
     <div class="social-link">
-      <a href={`whatsapp://send?text=${encodeURIComponent(url)}` }>
+      <a href={`whatsapp://send?text=${encodeURIComponent(url)}`}>
         <FaWhatsappSquare size={32} color={"#25D366"} />
       </a>
     </div>
     <div class="social-link">
-      <a href={`http://www.twitter.com/share?url=${encodeURIComponent(url)}` }>
+      <a href={`http://www.twitter.com/share?url=${encodeURIComponent(url)}`} target="_blank" rel="noreferrer">
         <FaTwitterSquare size={32} color={"#1DA1F2"} />
       </a>
     </div>
